cca604a20e497981cb7818ce368a2577f4691d25
thajacob <tha.jacob2@gmail.com>
2017-03-16 14:42:28 +0000

    Initial Commit

---
+++ squaresNewApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  squaresNewApp
+//
+//  Created by jakub skrzypczynski on 16/03/2017.
+//  Copyright © 2017 test project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
